15c3e539f73882da0d2f3d84bca63d70268ae7b9
Jess <jess@jessie.cafe>
2024-12-08 11:19:46 +1300

    dwm-status: track master
    master has fixes for an issue where network wouldn't render :3

--- modules/home/dwm-status/default.nix
+++ modules/home/dwm-status/default.nix
@@ -1,4 +1,9 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 let
   # https://github.com/nix-community/home-manager/blob/d00c6f6d0a/modules/services/dwm-status.nix#L10
   featureType = lib.types.enum [
@@ -26,6 +31,25 @@ in
 
   config.services.dwm-status = lib.mkIf cfg.enable {
     enable = true;
+
+    package = pkgs.dwm-status.overrideAttrs (
+      final: prev: {
+        # fixes for https://github.com/Gerschtli/dwm-status/issues/175
+        # TODO: Remove dwm-status override once network fix makes it into a release
+        src = pkgs.fetchFromGitHub {
+          owner = "Gerschtli";
+          repo = prev.pname;
+          rev = "8eede31a129de117b737fb0acf13f9ff453a295f";
+          sha256 = "sha256-VBd7htaj2Xk8zWZ7SmnKKt7QdkdXrO3M2K6vO8Evoug=";
+        };
+
+        cargoDeps = prev.cargoDeps.overrideAttrs {
+          inherit (final) src;
+
+          outputHash = "sha256-KDOcdzdfRwalCCRp96FToc+pFYGWU4RENgioun4qu0U=";
+        };
+      }
+    );
 
     order = cfg.features ++ [
       "network"
