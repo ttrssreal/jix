fb427114d2f5b7c7f560e924add52a9552228cd6
Jess <jess@jessie.cafe>
2024-12-23 14:11:15 +1300

    k8s/arc: init custom runner-image

---
+++ k8s/github-runners/runner-image/install_pkgs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -exo pipefail
+
+declare -r toolset=$1
+
+package_set=$(cat $toolset | jq -r "(.apt.vital_packages + .apt.common_packages + .apt.cmd_packages)[]")
+
+sudo apt-get install --no-install-recommends $package_set -y
