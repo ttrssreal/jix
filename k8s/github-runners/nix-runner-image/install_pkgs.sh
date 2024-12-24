888bc4a7168169237921947a9002af90d5618733
Jess <jess@jessie.cafe>
2024-12-24 14:16:27 +1300

    k8s/arc: install nix in image

--- k8s/github-runners/nix-runner-image/install_pkgs.sh
+++ k8s/github-runners/nix-runner-image/install_pkgs.sh
@@ -5,4 +5,4 @@ declare -r toolset=$1
 
 package_set=$(cat $toolset | jq -r "(.apt.vital_packages + .apt.common_packages + .apt.cmd_packages)[]")
 
-sudo apt-get install --no-install-recommends $package_set -y
+apt-get install --no-install-recommends $package_set -y
