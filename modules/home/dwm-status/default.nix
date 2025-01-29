5c9503b9b432ac94b5bb519fe181f3b3d07548c0
Jess <jess@jessie.cafe>
2025-01-30 04:47:52 +1300

    dwm-status: deprecate fork

--- modules/home/dwm-status/default.nix
+++ modules/home/dwm-status/default.nix
@@ -1,7 +1,6 @@
 {
   lib,
   config,
-  pkgs,
   ...
 }:
 let
@@ -36,27 +35,6 @@ in
 
   config.services.dwm-status = lib.mkIf cfg.enable {
     enable = true;
-
-    package = pkgs.dwm-status.overrideAttrs (
-      final: prev: {
-        # fixes for https://github.com/Gerschtli/dwm-status/issues/175
-        # implements https://github.com/Gerschtli/dwm-status/issues/177
-        # TODO: dwm-status: stop using fork once it reaches parity with upstream
-        # Issue URL: https://github.com/ttrssreal/jix/issues/10
-        src = pkgs.fetchFromGitHub {
-          owner = "ttrssreal";
-          repo = prev.pname;
-          rev = "b89daf4fda0a019f764e46d194e3b178f7a9d699";
-          sha256 = "sha256-Zi+SfsSWTnMYMdkfK+K/IKmy8rahDhjD3T6KJ+JL5rw=";
-        };
-
-        cargoDeps = prev.cargoDeps.overrideAttrs {
-          inherit (final) src;
-
-          outputHash = "sha256-dPqCyFWPqM/A2g23N1Of6BPijGMBu44xXSwVGO4j/Hk=";
-        };
-      }
-    );
 
     order = cfg.features ++ [
       "network"
