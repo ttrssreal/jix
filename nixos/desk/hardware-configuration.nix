c79ee33f1c99faa1ee7d70d67217cf1e88938e96
Jess <jess@jessie.cafe>
2025-10-14 17:39:09 +1300

    desk: add swapfile

--- nixos/desk/hardware-configuration.nix
+++ nixos/desk/hardware-configuration.nix
@@ -30,7 +30,12 @@
     fsType = "ext4";
   };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 16 * 1024; # 16 GB
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
