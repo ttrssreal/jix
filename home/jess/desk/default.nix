9531dd2c4831d70eb480bc3c87e69819903d4bd2
Jess <jess@jessie.cafe>
2025-10-21 23:09:14 +1300

    jess@desk: disable `enableDefaultConfig` before deprication

--- home/jess/desk/default.nix
+++ home/jess/desk/default.nix
@@ -75,6 +75,8 @@
 
           programs.ssh = {
             enable = true;
+            enableDefaultConfig = false;
+
             matchBlocks = {
               "adair" = {
                 remoteForwards = [
@@ -87,6 +89,19 @@
                     host.address = "/run/user/1000/gnupg/S.gpg-agent.extra";
                   }
                 ];
+              };
+
+              "*" = {
+                forwardAgent = false;
+                addKeysToAgent = "no";
+                compression = false;
+                serverAliveInterval = 0;
+                serverAliveCountMax = 3;
+                hashKnownHosts = false;
+                userKnownHostsFile = "~/.ssh/known_hosts";
+                controlMaster = "no";
+                controlPath = "~/.ssh/master-%r@%n:%p";
+                controlPersist = "no";
               };
             };
           };
