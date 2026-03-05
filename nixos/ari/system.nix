d04789815615a0c69d5e8f88e63ff9dad1f26212
Jess <jess@jessie.cafe>
2026-03-05 19:42:21 +1300

    ari: use dhcp instead of static addresses

--- nixos/ari/system.nix
+++ nixos/ari/system.nix
@@ -58,22 +58,6 @@
   networking = {
     networkmanager.enable = true;
     firewall.enable = false;
-
-    interfaces.eno1 = {
-      ipv4.addresses = [
-        {
-          address = "192.168.150.2";
-          prefixLength = 24;
-        }
-      ];
-    };
-
-    defaultGateway = {
-      address = "192.168.150.1";
-      interface = "eno1";
-    };
-
-    nameservers = [ "192.168.150.1" ];
   };
 
   boot.loader.grub = {
