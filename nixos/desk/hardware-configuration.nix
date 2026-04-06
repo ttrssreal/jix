b8c73e6ed5ff81fef51dd73087be93cb40c15717
Jess <jess@jessie.cafe>
2026-04-06 20:13:20 +1200

    desk: don't use a swapfile anymore

--- nixos/desk/hardware-configuration.nix
+++ nixos/desk/hardware-configuration.nix
@@ -30,12 +30,7 @@
     fsType = "ext4";
   };
 
-  swapDevices = [
-    {
-      device = "/var/lib/swapfile";
-      size = 16 * 1024; # 16 GB
-    }
-  ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
